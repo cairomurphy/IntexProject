387b8abbf2539dc6081ccd99cff188b03e42fcd0
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-04-04 11:33:45 -0600

    Add project files.

---
+++ IntexProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IntexProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
